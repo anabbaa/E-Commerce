1c74b55d99709addd3e62d857bf4b969299cce6b
anabbaa <ahmed.nabaa.na@gmail.com>
2026-06-21 14:18:12 +0200

    sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,42 @@
-import React from 'react'
-import Home from './components/Home'
-import { useProducts} from './context/CartContext'
-import Cart from './components/Cart'
-import { createBrowserRouter , RouterProvider} from 'react-router-dom'
-import ProductsList from './components/ProductsList'
-import Layout from './components/LayOut'
-import Load from './components/Load'
-import ViewMore from './components/ViewMore'
+import React from "react";
+import Home from "./components/Home";
+import { useProducts } from "./context/CartContext";
+import Cart from "./components/Cart";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/LayOut";
+import ViewMore from "./components/ViewMore";
 
 const App = () => {
-const { handelAddToCart} = useProducts();
+  const { handelAddToCart } = useProducts();
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <Layout>
-        <Home onAddToCart={handelAddToCart} />
-      </Layout>
-    ),
-  },
-  {
-    path: "/cart",
-    element: (
-      <Layout>
-        <Cart onAddToCart={handelAddToCart} 
-/>
-      </Layout>
-    ),
-  },
-  {
-    path: "/viewmore/:id",
-    element : (
-      <Layout>
-        <ViewMore  onAddToCart={handelAddToCart}/>
-      </Layout>
-    )
-  }
-]);
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: (
+        <Layout>
+          <Home onAddToCart={handelAddToCart} />
+        </Layout>
+      ),
+    },
+    {
+      path: "/cart",
+      element: (
+        <Layout>
+          <Cart onAddToCart={handelAddToCart} />
+        </Layout>
+      ),
+    },
+    {
+      path: "/viewmore/:id",
+      element: (
+        <Layout>
+          <ViewMore onAddToCart={handelAddToCart} />
+        </Layout>
+      ),
+    },
+  ]);
 
-  return (
-    <>    
-    <RouterProvider  router={router}  ></RouterProvider>
-  
-    </>
-  
-      )  
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
