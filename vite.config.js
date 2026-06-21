71a1f076ddc515aca1601bf9503ca9538917cb54
anabbaa <ahmed.nabaa.na@gmail.com>
2026-06-21 14:01:43 +0200

    sec

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import React from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
- // server: {
-  //  port: 5000, 
-   // open: true
-  //}
+  base: "/E-Commerce/",
 
-})
+  // server: {
+  //   port: 5000,
+  //   open: true,
+  // },
+});
